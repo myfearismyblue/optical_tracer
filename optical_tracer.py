8da0d73cbbeff47fa0b70ad122291d225a3a0a85
Andrey Smekhov <myfearismyblue@gmail.com>
2022-05-11 23:26:34 +0300

    Fix bug with Layer.contains_point
    Bug occurred when point is located at the right border of a left-handed material

--- optical_tracer.py
+++ optical_tracer.py
@@ -362,7 +362,7 @@ class Layer:
         Checks if input point and material (self.side) are at a same side of boundary
         """
         point_right = point.z >= self.boundary(point.y)
-        point_left = not point_right
+        point_left = point.z <= self.boundary(point.y)
         if (point_right and self.side == Side.RIGHT) or (point_left and self.side == Side.LEFT):
             return True
         return False
