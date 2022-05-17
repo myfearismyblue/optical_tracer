a2fb46c0a1f6f5f0a824a077c09c6d83e9a56a75
Andrey Smekhov <myfearismyblue@gmail.com>
2022-05-17 23:12:44 +0300

    Small refactoring
    Add TOLL global instead of hardcoded tolerance in pytest.approx
    Direct import of approx from pytest

--- tests/test_OpticalSystem.py
+++ tests/test_OpticalSystem.py
@@ -1,19 +1,21 @@
 import pytest
+from pytest import approx
+
 from optical_tracer import Layer, Material, OpticalComponent, OpticalSystem, Point
 from optical_tracer import reversed_side, Side, Vector, VectorOutOfComponentException
-from math import pi
 
 deg = 2 * pi / 360
 Air = Material(name="Air", transmittance=0, refractive_index=1)
 opt_c = OpticalComponent(name='first lense')
+TOLL = 10 ** -2
 
 
 @pytest.mark.parametrize('vector_angle, normal_angle, refractive_index1, refractive_index2, expected',
-                         [(190 * deg, 145 * deg, 1, 1.5, pytest.approx(173.1255 * deg, 0.001)),
-                          (325 * deg, 10 * deg, 1, 1.5, pytest.approx(341.8744 * deg, 0.001)),
-                          (0 * deg, 0 * deg, 1, 1.5, pytest.approx(0 * deg, 0.001)),
-                          (190 * deg, 85 * deg, 1, 1, pytest.approx(190 * deg, 0.001)),
-                          (180 * deg, 0 * deg, 1, 1, pytest.approx(180 * deg, 0.001)),
+                         [(190 * deg, 145 * deg, 1, 1.5, approx(173.1255 * deg, TOLL)),
+                          (325 * deg, 10 * deg, 1, 1.5, approx(341.8744 * deg, TOLL)),
+                          (0 * deg, 0 * deg, 1, 1.5, approx(0 * deg, TOLL)),
+                          (190 * deg, 85 * deg, 1, 1, approx(190 * deg, TOLL)),
+                          (180 * deg, 0 * deg, 1, 1, approx(180 * deg, TOLL)),
                           ]
                          )
 def test__get_refract_angle(vector_angle, normal_angle, refractive_index1, refractive_index2, expected):
@@ -45,12 +47,12 @@ intersec = [(parabolic_l, Point(x=0, y=0.0001, z=0)),
 
 
 @pytest.mark.parametrize('intersec, expected',
-                         [(intersec[0], pytest.approx(3.1413926, 0.001)),  # ~<pi - approaching zero from plus
-                          (intersec[1], pytest.approx(0, 0.001)),  # parallel to z-axis
-                          (intersec[2], pytest.approx(0.0001999, 0.001)),  # approaching zero from minus
-                          (intersec[3], pytest.approx(1.575796285141478, 0.001)),  # almost perpendicular
-                          (intersec[4], pytest.approx(1.5657963684483152, 0.001)),  # same approaching from another side
-                          (intersec[5], pytest.approx(0, 0.001)),  # check with constant (plane) bound
+                         [(intersec[0], approx(3.1413926, TOLL)),  # ~<pi - approaching zero from plus
+                          (intersec[1], approx(0, TOLL)),  # parallel to z-axis
+                          (intersec[2], approx(0.0001999, TOLL)),  # approaching zero from minus
+                          (intersec[3], approx(1.575796285141478, TOLL)),  # almost perpendicular
+                          (intersec[4], approx(1.5657963684483152, TOLL)),  # same approaching from another side
+                          (intersec[5], approx(0, TOLL)),  # check with constant (plane) bound
                           ])
 def test__get_normal_angle(intersec, expected):
     assert opt_c._get_normal_angle(intersection=intersec) == expected
