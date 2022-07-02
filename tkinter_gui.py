2febf0e3a41353b0ad8adbe9c17e2e835e03d74a
Andrey Smekhov <myfearismyblue@gmail.com>
2022-07-02 16:10:01 +0200

    Refactor zooming way

--- tkinter_gui.py
+++ tkinter_gui.py
@@ -8,15 +8,19 @@ DEBUG = False
 CANVAS_WIDTH = 800
 CANVAS_HEIGHT = 600
 CANVAS_BACKGROUND_COLOR = 'white'
-TIME_REFRESH = 100 # ms
-SCALE = 1 # mm/px
-WHEEL_ZOOM_RATIO = 1.1      # while zooming with mouse wheel
+TIME_REFRESH = 100  # ms
+SCALE = 1  # mm/px
+ZOOMIN_TK_EVENT = '<Button-1>'  # you can change this while running on win or macos
+ZOOMOUT_TK_EVENT = '<Button-2>'  # you can change this while running on win or macos
+WHEEL_ZOOM_RATIO = 1.1  # while zooming with mouse wheel
 
 # offset of entire optical system relatively to (0, 0) canvas point which is upper-left corner
-OPTICAL_SYSTEM_OFFSET = (+1*CANVAS_WIDTH//3, +1*CANVAS_HEIGHT//3)     # in pixels here
+OPTICAL_SYSTEM_OFFSET = (+1 * CANVAS_WIDTH // 3, +1 * CANVAS_HEIGHT // 3)  # in pixels here
 
-# ranges in which components to be drawn relatively to OPTICAL_SYSTEM_OFFSET in pixeles here
-BOUNDARY_DRAW_RANGES = (-OPTICAL_SYSTEM_OFFSET[1], (CANVAS_HEIGHT - OPTICAL_SYSTEM_OFFSET[1]))
+# ranges in which components to be drawn relatively to OPTICAL_SYSTEM_OFFSET in pixels here
+# coordinates are upside-down because of tkinter vertical axes is reverted
+BOUNDARY_DRAW_RANGES = (OPTICAL_SYSTEM_OFFSET[1] - CANVAS_HEIGHT, OPTICAL_SYSTEM_OFFSET[1])
+
 
 def main():
     global root, canvas
@@ -25,17 +29,19 @@ def main():
     root = tk.Tk()
     root.geometry(str(CANVAS_WIDTH) + 'x' + str(CANVAS_HEIGHT))
     canvas = tk.Canvas(root, background=CANVAS_BACKGROUND_COLOR)
-    mouse_coords_text = canvas.create_text(2, 2, text=f'', font='28', justify='left', anchor='nw', tags='upper_coords')
-
+    mouse_coords_text = canvas.create_text(2, 2, text=f'', font='28', justify='left', anchor='nw')
+    canvas.itemconfig(mouse_coords_text, tags=('upper_coords', 'testtag'))
     canvas.focus_set()
 
     view_obj = (Grapher(canvas=canvas, opt_system=model_objects[0]),)
 
     canvas.bind('<Motion>', lambda event: key_handler(event, *view_obj))
-    canvas.bind('<MouseWheel>', lambda event: key_handler(event, *view_obj))
+    canvas.bind(ZOOMIN_TK_EVENT, lambda event: key_handler(event, *view_obj))
+    canvas.bind(ZOOMOUT_TK_EVENT, lambda event: key_handler(event, *view_obj))
     canvas.pack(fill=tk.BOTH, expand=1)
     # tick(*objects)
     root.mainloop()
+
 
 def convert_tkcoords_to_optical(tk_absciss: int, tk_ordinate: int, *, scale: float) -> Tuple[float, float]:
     """ Returns real coordinates in mm
@@ -45,13 +51,19 @@ def convert_tkcoords_to_optical(tk_absciss: int, tk_ordinate: int, *, scale: flo
     opt_ordinate = (OPTICAL_SYSTEM_OFFSET[1] - tk_ordinate) / scale
     return opt_absciss, opt_ordinate
 
-def convert_opticalcoords_to_tkcoords(opt_absciss: int, opt_ordinate: int, *, scale: float) -> Tuple[float, float]:
-    """ Returns coordinates oncanvas
+
+def convert_opticalcoords_to_tkcoords(opt_absciss: float, opt_ordinate: float, *,
+                                      scale: float,
+                                      absciss_offset: int = OPTICAL_SYSTEM_OFFSET[0],
+                                      ordinate_offset: int = OPTICAL_SYSTEM_OFFSET[1]
+                                      ) -> Tuple[float, float]:
+    """ Returns coordinates on canvas
         scale - in pixels per mm
         """
-    tk_absciss = opt_absciss * scale + OPTICAL_SYSTEM_OFFSET[0]
-    tk_ordinate = OPTICAL_SYSTEM_OFFSET[1] - opt_ordinate * scale
+    tk_absciss = opt_absciss * scale + absciss_offset
+    tk_ordinate = ordinate_offset - opt_ordinate * scale
     return tk_absciss, tk_ordinate
+
 
 def tick(*objects):
     root.after(TIME_REFRESH, *objects)
@@ -83,11 +95,13 @@ def key_handler(event, *view_obj):
         opt_abs, opt_ord = convert_tkcoords_to_optical(event.x, event.y, scale=SCALE)
         canvas.itemconfig(mouse_coords, text=f'y: {opt_ord}, mm, z: {opt_abs}, mm')
 
-    elif str(event.type) in ['MouseWheel', '38']:
-        if event.delta >= 0:
+    elif str(event.type) == '4':  # mouse button pressed
+        if event.num == 1:
             gr._scale *= WHEEL_ZOOM_RATIO
+            gr._offset = [int(_ * gr._scale) for _ in gr._offset]
         else:
             gr._scale /= WHEEL_ZOOM_RATIO
+            gr._offset = [int(_ / gr._scale) for _ in gr._offset]
         gr._refresh_canvas()
 
 
@@ -142,13 +156,15 @@ def init_model_objects():
                                                                     )
         [opt_sys.add_component(component=med) for med in (first_medium, second_medium, third_medium, fourth_medium)]
         return opt_sys
+
     opt_sys = create_opt_sys()
-    in_point = Point(x=0, y=100, z=2)
-    vs = (Vector(initial_point=in_point, lum=1, w_length=555, theta=t/20, psi=0) for t in range(0, (int(2*pi)*20)))
+    in_point = Point(x=0, y=0, z=0)
+    vs = (Vector(initial_point=in_point, lum=1, w_length=555, theta=t / 20, psi=0) for t in
+          range(0, (int(2 * pi) * 20)))
 
     for v in vs:
-        # if v.theta == 0.65:
-        opt_sys.trace(vector=v)
+        if v.theta == 0.65:
+            opt_sys.trace(vector=v)
     objects = (opt_sys,)
     print('')
     return objects
@@ -156,10 +172,12 @@ def init_model_objects():
 
 class Grapher:
     """Responsible for drawing and refreshing visualities on a canvas"""
+
     def __init__(self, *, canvas, opt_system):
         self._canvas = canvas
         self._optical_system = opt_system
         self._scale = SCALE
+        self._offset = (OPTICAL_SYSTEM_OFFSET[0], OPTICAL_SYSTEM_OFFSET[1])
         self._refresh_canvas()
 
     def _refresh_canvas(self):
@@ -169,27 +187,29 @@ class Grapher:
         self._draw_beams()
 
     def _draw_initial_axes(self):
-        optical_axis_points = 0, \
-                              0 + OPTICAL_SYSTEM_OFFSET[1], \
+        absciss_axis_points = 0, \
+                              0 + self._offset[1], \
                               CANVAS_WIDTH, \
-                              OPTICAL_SYSTEM_OFFSET[1]
+                              self._offset[1]
 
-        y_axis_points = OPTICAL_SYSTEM_OFFSET[0], \
-                        0, \
-                        OPTICAL_SYSTEM_OFFSET[0], \
-                        CANVAS_HEIGHT
+        ordinate_axis_points = self._offset[0], \
+                               0, \
+                               self._offset[0], \
+                               CANVAS_HEIGHT
 
-        self._canvas.create_line(*optical_axis_points, arrow=tk.LAST)
-        self._canvas.create_line(*y_axis_points, arrow=tk.FIRST)
+        self._canvas.create_line(*absciss_axis_points, arrow=tk.LAST)
+        self._canvas.create_line(*ordinate_axis_points, arrow=tk.FIRST)
 
     def _draw_components(self):
         """Draws components, it's boundaries, material etc."""
+
         def _fetch_boundaries():
             res = []
             for comp in self._optical_system._components:
                 for l in comp._layers:
-                   res.append(l.boundary)
+                    res.append(l.boundary)
             return res
+
         boundaries = _fetch_boundaries()
         for bound in boundaries:
             self._draw_layers_bounds(bound)
@@ -198,7 +218,10 @@ class Grapher:
         assert isinstance(boundary_func, Callable)
         ys_in_mm = (el * self._scale for el in range(BOUNDARY_DRAW_RANGES[0], BOUNDARY_DRAW_RANGES[1]))
         zs_in_mm = (boundary_func(y) for y in ys_in_mm)
-        points_to_draw = list(convert_opticalcoords_to_tkcoords(z, y, scale=self._scale) for z, y in zip(zs_in_mm, ys_in_mm))
+        points_to_draw = list(convert_opticalcoords_to_tkcoords(z, y, scale=self._scale,
+                                                                absciss_offset=self._offset[0],
+                                                                ordinate_offset=self._offset[1]
+                                                                ) for z, y in zip(zs_in_mm, ys_in_mm))
         self._canvas.create_line(*points_to_draw, smooth=True)
 
     def _draw_beams(self):
@@ -210,15 +233,20 @@ class Grapher:
     def _draw_beam(self, beam):
         """Draws a single beam propagating throw optical system"""
         zs_in_mm, ys_in_mm, ts = ([] for _ in range(3))
-        [(zs_in_mm.append(vector.initial_point.z), ys_in_mm.append(vector.initial_point.y), ts.append(vector.theta)) for vector in beam]
+        [(zs_in_mm.append(vector.initial_point.z), ys_in_mm.append(vector.initial_point.y), ts.append(vector.theta)) for
+         vector in beam]
         last_point_y = beam[-1].initial_point.y
         last_point_z = beam[-1].initial_point.z
         zs_in_mm.append(last_point_z)
         ys_in_mm.append(last_point_y)
         ts.append(ts[-1])
         # points_mm = list(zip(zs_in_mm, ys_in_mm))
-        points_to_draw = [convert_opticalcoords_to_tkcoords(z, y, scale=self._scale) for z, y in zip(zs_in_mm, ys_in_mm)]
+        points_to_draw = [convert_opticalcoords_to_tkcoords(z, y, scale=self._scale,
+                                                            absciss_offset=self._offset[0],
+                                                            ordinate_offset=self._offset[1]) for z, y in
+                          zip(zs_in_mm, ys_in_mm)]
         self._canvas.create_line(points_to_draw, arrow='last', fill='blue')
+
         # if len(points_to_draw)>=2:
         #     self._canvas.create_text(points_to_draw[1], text=f'{ts[1]}', font='28', justify='left', anchor='nw')
         def create_point(x, y, canvas, r=2):
@@ -226,6 +254,7 @@ class Grapher:
             x1, y1 = (x - r), (y - r)
             x2, y2 = (x + r), (y + r)
             canvas.create_oval(x1, y1, x2, y2, fill=python_green)
+
         [create_point(*point, self._canvas) for point in points_to_draw]
         # for i in range(len(points_to_draw)):
         self._canvas.create_text(*points_to_draw[-1], text=f' {ts[0]:.2f}', font='28', justify='left', anchor='nw')
@@ -233,9 +262,6 @@ class Grapher:
     @staticmethod
     def _wavelength_to_rgb(wavelength, gamma=0.8):
         ...
-
-
-
 
 
 if __name__ == '__main__':
