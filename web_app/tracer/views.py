84bdea1fa7217d6aec6e3e390648d6b0d55e7bfa
Andrey Smekhov <myfearismyblue@gmail.com>
2022-09-08 19:50:16 +0300

    Fix issue in views.index with instancing forms in a proper way if other form had been submitted

--- web_app/tracer/views.py
+++ web_app/tracer/views.py
@@ -9,35 +9,41 @@ from .services import ContextRequest, GraphService, FormHandleBaseStrategy, AddC
 
 
 def index(request):
+    # FIXME: suppose there is a better way of such handling
+    try:
+        current_opt_sys_id: Optional[int] = int(request.COOKIES.get('current_opt_sys_id'))
+    except ValueError:
+        current_opt_sys_id = None
+
+    if request.method == 'POST':
+        add_component_form: forms.Form = AddComponentForm(request.POST)
+        choose_optical_system_form: forms.Form = ChooseOpticalSystemForm(request.POST)
+
+        # FIXME: make a convenient way of choosing strategy of form handling
+        if add_component_form.is_valid():
+            form_handler: FormHandleBaseStrategy = AddComponentFormHandleService(opt_sys_id=current_opt_sys_id)
+            form_handler.handle(add_component_form)
+            # if next form is submitted, let instance this one in a correct way
+            choose_optical_system_form: forms.Form = ChooseOpticalSystemForm()
+        if choose_optical_system_form.is_valid():
+            form_handler: FormHandleBaseStrategy = ChooseOpticalSystemFormHandleService(opt_sys_id=current_opt_sys_id)
+            form_handler.handle(choose_optical_system_form)
+            # if previous form is submitted, let instance this one in a correct way
+            add_component_form: forms.Form = AddComponentForm()
+        push_sides_to_db_if_not_exist()     # sides are needed in add optical component form
+        current_optical_system = form_handler.builder.optical_system
+        current_opt_sys_id = form_handler.optical_system_id
+    else:
+        add_component_form: forms.Form = AddComponentForm()
+        choose_optical_system_form: forms.Form = ChooseOpticalSystemForm()
+        current_optical_system = fetch_optical_system_by_id(id=current_opt_sys_id)
+
     graph_info = {'canvas_width': 1600,
                   'canvas_height': 1200,
                   'scale': 1,
                   }
     contexts_list = ['canvas_context', 'axis_context', 'boundaries_context', 'beams_context']
     contexts_request = ContextRequest(contexts_list=contexts_list, graph_info=graph_info)
-    add_component_form: forms.Form = AddComponentForm(request.POST or None)
-
-    # FIXME: suppose there is a better way of such handling
-    try:
-        current_opt_sys_id: Optional[int] = int(request.COOKIES.get('current_opt_sys_id'))
-    except ValueError:
-        current_opt_sys_id = None
-    choose_optical_system_form: forms.Form = ChooseOpticalSystemForm(request.POST or None)
-
-    if request.method == 'POST':
-        # FIXME: make a convenient way of choosing strategy of form handling
-        if add_component_form.is_valid():
-            form_handler: FormHandleBaseStrategy = AddComponentFormHandleService(opt_sys_id=current_opt_sys_id)
-            form_handler.handle(add_component_form)
-        if choose_optical_system_form.is_valid():
-            form_handler: FormHandleBaseStrategy = ChooseOpticalSystemFormHandleService(opt_sys_id=current_opt_sys_id)
-            form_handler.handle(choose_optical_system_form)
-        push_sides_to_db_if_not_exist()     # sides are needed in add optical component form
-        current_optical_system = form_handler.builder.optical_system
-        current_opt_sys_id = form_handler.optical_system_id
-    else:
-        current_optical_system = fetch_optical_system_by_id(id=current_opt_sys_id)
-
     gr_service = GraphService(contexts_request=contexts_request,
                               optical_system=current_optical_system)
 
